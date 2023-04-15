b9d45e75f16746210df5f1be41c795dbf633adae
caner-cakar <cakar_caner@outlook.com>
2023-04-15 16:58:45 +0300

    Script improvements

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,22 @@ public class GameController : MonoBehaviour
 {
     public List<Button> btns = new List<Button>();
     [SerializeField] private Sprite bgImage;
+
+    public Sprite[] puzzles;
+    public List<Sprite> gamePuzzles = new List<Sprite>();
+    int lastValue;
+
+    private bool firstGuess,secondGuess;
+
+    private void Awake() 
+    {
+        puzzles = Resources.LoadAll<Sprite>("Sprites/Cards");      
+    }
     
     void Start()
     {
         GetButton();
+        AddGamePuzzle();
     }
 
     void GetButton()
@@ -24,4 +36,42 @@ public class GameController : MonoBehaviour
         }   
     }
 
+    private void AddGamePuzzle()
+    {
+        int index = 0;
+        for (int i = 0; i < btns.Count; i++)
+        {
+            if(index == btns.Count /2 )
+            {
+                index =0;
+            }
+            gamePuzzles.Add(puzzles[index]);
+            index++;
+        }
+        shufle(gamePuzzles);
+    }
+
+    private void shufle(List<Sprite> newGamePuzzle)
+    {
+        for (int i = 0; i < newGamePuzzle.Count; i++)
+        {
+            Sprite temp = newGamePuzzle[i];
+            int rand = Random.Range(0,newGamePuzzle.Count);
+            newGamePuzzle[i]= newGamePuzzle[rand];   
+            newGamePuzzle[rand] = temp;
+        }
+    }
+
+
+    private void AddListeners()
+    {
+        foreach (Button buttons in btns)
+        {
+            buttons.onClick.AddListener(() => PickaPuzzle());
+        }
+    }
+    private void PickaPuzzle()
+    {
+
+    }
 }
